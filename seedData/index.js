6ff5ee4cc7dea8991a0ae7f95c9785ab269ee577
judgemnt <61425299+judgemnt@users.noreply.github.com>
2021-03-01 15:24:29 -0500

    Updated database seed
    - Updated seed data to work with current schemas.

--- seedData/index.js
+++ seedData/index.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const Workout = require("../models/workoutSchema");
 const Program = require("../models/programSchema");
+const Exercise = require("../models/exerciseSchema")
 const { exercises, sets, reps } = require("./exercises.js");
 
 mongoose.connect('mongodb://localhost:27017/workoutLog', {
@@ -19,65 +20,35 @@ db.once("open", () => {
 const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
-    await Workout.deleteMany({});
     await Program.deleteMany({});
-    for (let i = 0; i < 6; i++) {
-        const workout = new Workout({
-            session: `Day ${i}`,
-            exercises: [{
-                exercise: `${sample(exercises)}`,
-                sets: `${sample(sets)}`,
-                reps: `${sample(reps)}`
-            }, {
-                exercise: `${sample(exercises)}`,
-                sets: `${sample(sets)}`,
-                reps: `${sample(reps)}`
-            }, {
-                exercise: `${sample(exercises)}`,
-                sets: `${sample(sets)}`,
-                reps: `${sample(reps)}`
-            }, {
-                exercise: `${sample(exercises)}`,
-                sets: `${sample(sets)}`,
-                reps: `${sample(reps)}`
-            }, {
-                exercise: `${sample(exercises)}`,
-                sets: `${sample(sets)}`,
-                reps: `${sample(reps)}`
-            }, {
-                exercise: `${sample(exercises)}`,
-                sets: `${sample(sets)}`,
-                reps: `${sample(reps)}`
-            }, {
-                exercise: `${sample(exercises)}`,
-                sets: `${sample(sets)}`,
-                reps: `${sample(reps)}`
-            }, {
-                exercise: `${sample(exercises)}`,
-                sets: `${sample(sets)}`,
-                reps: `${sample(reps)}`
-            }, {
-                exercise: `${sample(exercises)}`,
-                sets: `${sample(sets)}`,
-                reps: `${sample(reps)}`
-            }, {
-                exercise: `${sample(exercises)}`,
-                sets: `${sample(sets)}`,
-                reps: `${sample(reps)}`
-            }],
-            description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.Omnis assumenda adipisci animi soluta sunt eum non illum quidem dolores, minus facilis natus dolorem reprehenderit vero neque! Nostrum, rerum! Neque, iste!"
-        });
-        await workout.save();
+    await Workout.deleteMany({});
+    await Exercise.deleteMany({});
+    for (let i = 0; i < 5; i++) {
         const program = new Program({
-            title: `Strong boi ${i}`,
-            workouts: [workout],
-            description: "this works"
+            title: `program${[i]}`,
+            description: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.",
         });
         await program.save();
+        const workout = new Workout({
+            session: `Session${i}`,
+            comments: "none",
+            description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            program: program._id,
+        });
+        await workout.save();
+        await program.updateOne({ $push: { workouts: workout._id } });
+        const exercise = new Exercise({
+            exercise: `${sample(exercises)}`,
+            sets: `${sample(sets)}`,
+            reps: `${sample(reps)}`,
+            workout: workout._id
+        });
+        await exercise.save();
+        await workout.updateOne({ $push: { exercises: exercise._id } });
     };
 };
 
-
 seedDB().then(() => {
+
     mongoose.connection.close();
 })
